cf27990049f89a750a56324b0ad06037f974ef0c
naveen4644 <59799669+naveen4644@users.noreply.github.com>
2023-05-13 10:49:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "aws_security_group" "allow_all" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["192.168.25.0/24]
   }
 
   egress {
